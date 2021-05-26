4f2ddeaf65abf7156df3d1a6c9a8f2cd6cbb14e5
Takuya Kitazawa <k.takuti@gmail.com>
2021-05-25 18:34:44 -0700

    Solve: 0097 - Optimize w/ mamoization

--- 0097/solution.py
+++ 0097/solution.py
@@ -1,38 +1,21 @@
 class Solution(object):
-    def isInterleave(self, s1, s2, s3):
-        """
-        :type s1: str
-        :type s2: str
-        :type s3: str
-        :rtype: bool
-        """
-        if len(s1) + len(s2) != len(s3):
+    def isInterleave_(self, s1, i, s2, j, s3, k, memo):
+        if len(s1[i:]) + len(s2[j:]) != len(s3[k:]):
             return False
 
-        if len(s3) <= 1:
-            return s1 + s2 == s3
-
-        cnt = {}
-        for c in (s1 + s2):
-            if c not in cnt:
-                cnt[c] = 0
-            cnt[c] += 1
-        for c in s3:
-            if c not in cnt or cnt[c] == 0:
-                return False
-            cnt[c] -= 1
-
-        i, j, k = 0, 0, 0
-
         while i < len(s1) and j < len(s2) and k < len(s3):
+            if memo[i][j] != -1:
+                return memo[i][j] == 1
+
             if s3[k] != s1[i] and s3[k] != s2[j]:
+                memo[i][j] = 0
                 return False
 
             if s1[i] == s2[j]:
-                if self.isInterleave(s1[i+1:], s2[j:], s3[k+1:]):
+                if self.isInterleave_(s1, i+1, s2, j, s3, k+1, memo) or \
+                    self.isInterleave_(s1, i, s2, j+1, s3, k+1, memo) :
+                    memo[i][j] = 1
                     return True
-                else:
-                    j += 1
             elif s3[k] == s1[i]:
                 i += 1
             else:
@@ -41,3 +24,15 @@ class Solution(object):
             k += 1
 
         return s1[i:] + s2[j:] == s3[k:]
+
+    def isInterleave(self, s1, s2, s3):
+        """
+        :type s1: str
+        :type s2: str
+        :type s3: str
+        :rtype: bool
+        """
+        memo = [[-1] * len(s2) for _ in range(len(s1))]
+
+        return self.isInterleave_(s1, 0, s2, 0, s3, 0, memo)
+
