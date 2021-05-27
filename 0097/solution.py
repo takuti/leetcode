294fc4d51c48dca68b8e1b3642f851e5a6ebd5dc
Takuya Kitazawa <k.takuti@gmail.com>
2021-05-26 22:18:32 -0700

    Solve with DP

--- 0097/solution.py
+++ 0097/solution.py
@@ -1,30 +1,4 @@
 class Solution(object):
-    def isInterleave_(self, s1, i, s2, j, s3, k, memo):
-        if len(s1[i:]) + len(s2[j:]) != len(s3[k:]):
-            return False
-
-        while i < len(s1) and j < len(s2) and k < len(s3):
-            if memo[i][j] != -1:
-                return memo[i][j] == 1
-
-            if s3[k] != s1[i] and s3[k] != s2[j]:
-                memo[i][j] = 0
-                return False
-
-            if s1[i] == s2[j]:
-                if self.isInterleave_(s1, i+1, s2, j, s3, k+1, memo) or \
-                    self.isInterleave_(s1, i, s2, j+1, s3, k+1, memo) :
-                    memo[i][j] = 1
-                    return True
-            elif s3[k] == s1[i]:
-                i += 1
-            else:
-                j += 1
-
-            k += 1
-
-        return s1[i:] + s2[j:] == s3[k:]
-
     def isInterleave(self, s1, s2, s3):
         """
         :type s1: str
@@ -32,7 +6,25 @@ class Solution(object):
         :type s3: str
         :rtype: bool
         """
-        memo = [[-1] * len(s2) for _ in range(len(s1))]
+        if len(s1) + len(s2) != len(s3):
+            return False
 
-        return self.isInterleave_(s1, 0, s2, 0, s3, 0, memo)
+        #     _ s21 s22 s23 ...
+        #   _
+        # s11
+        # s12
+        # s13
+        # ...
+        dp = [[False] * (len(s2) + 1) for _ in range(len(s1) + 1)]
+        for i in range(len(s1) + 1):
+            for j in range(len(s2) + 1):
+                if i == 0 and j == 0:
+                    dp[i][j] = True
+                elif i == 0:
+                    dp[i][j] = dp[i][j-1] and s2[j-1] == s3[j-1]
+                elif j == 0:
+                    dp[i][j] = dp[i-1][j] and s1[i-1] == s3[i-1]
+                else:
+                    dp[i][j] = (dp[i-1][j] and s1[i-1] == s3[i+j-1]) or (dp[i][j-1] and s2[j-1] == s3[i+j-1])
 
+        return dp[-1][-1]
