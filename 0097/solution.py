73050b8e9a95869b8a342f0e0230482289b67288
Takuya Kitazawa <k.takuti@gmail.com>
2021-05-26 22:21:55 -0700

    1D dp

--- 0097/solution.py
+++ 0097/solution.py
@@ -9,22 +9,16 @@ class Solution(object):
         if len(s1) + len(s2) != len(s3):
             return False
 
-        #     _ s21 s22 s23 ...
-        #   _
-        # s11
-        # s12
-        # s13
-        # ...
-        dp = [[False] * (len(s2) + 1) for _ in range(len(s1) + 1)]
+        dp = [False] * (len(s2) + 1)
         for i in range(len(s1) + 1):
             for j in range(len(s2) + 1):
                 if i == 0 and j == 0:
-                    dp[i][j] = True
+                    dp[j] = True
                 elif i == 0:
-                    dp[i][j] = dp[i][j-1] and s2[j-1] == s3[j-1]
+                    dp[j] = dp[j-1] and s2[j-1] == s3[j-1]
                 elif j == 0:
-                    dp[i][j] = dp[i-1][j] and s1[i-1] == s3[i-1]
+                    dp[j] = dp[j] and s1[i-1] == s3[i-1]
                 else:
-                    dp[i][j] = (dp[i-1][j] and s1[i-1] == s3[i+j-1]) or (dp[i][j-1] and s2[j-1] == s3[i+j-1])
+                    dp[j] = (dp[j] and s1[i-1] == s3[i+j-1]) or (dp[j-1] and s2[j-1] == s3[i+j-1])
 
-        return dp[-1][-1]
+        return dp[-1]
